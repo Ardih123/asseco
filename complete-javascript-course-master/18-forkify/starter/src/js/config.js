583a3945f978da5e2fcf9bca8eb954d7b4a4954e
Ardih <joaoandrade888@gmail.com>
2022-05-18 08:39:50 +0100

    change place

--- complete-javascript-course-master/18-forkify/starter/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '31c3beea-8696-4c5d-8732-449b10c2625e';
-export const MODAL_CLOSE_SECONDS = 2.5;
